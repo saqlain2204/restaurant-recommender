a139ed86437616cf119eb9844d0f5ba835697b91
M Saqlain <118016760+saqlain2204@users.noreply.github.com>
2023-09-18 19:52:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -45,20 +45,13 @@ index = fetch_index()
 # initialising the chat_engine with a `context` chatmode that takes into account the chat history while responding to user query.
 
 
-    memory = ChatMemoryBuffer.from_defaults(token_limit=2500)
-    chat_engine = index.as_chat_engine(
-        chat_mode="context",
-        memory = memory,
-        system_prompt="Assist in offering tailored hotel recommendations based on user inquiries, taking into account the desired location, budget constraints, and individual preferences. Ensure to present the most suitable options with comprehensive details regarding amenities, user reviews, and real-time availability. If you have access to URLs or web links associated with these recommendations, please provide them when available."
-        )
-    st.write("Sorry the api key has expired. Please enter you own api key to continue")
-    openai.api_key = st.text_area("Enter you key api key here")
-    memory = ChatMemoryBuffer.from_defaults(token_limit=2500)
-    chat_engine = index.as_chat_engine(
-        chat_mode="context",
-        memory = memory,
-        system_prompt="Assist in offering tailored hotel recommendations based on user inquiries, taking into account the desired location, budget constraints, and individual preferences. Ensure to present the most suitable options with comprehensive details regarding amenities, user reviews, and real-time availability. If you have access to URLs or web links associated with these recommendations, please provide them when available."
-        )
+memory = ChatMemoryBuffer.from_defaults(token_limit=2500)
+chat_engine = index.as_chat_engine(
+    chat_mode="context",
+    memory = memory,
+    system_prompt="Assist in offering tailored hotel recommendations based on user inquiries, taking into account the desired location, budget constraints, and individual preferences. Ensure to present the most suitable options with comprehensive details regarding amenities, user reviews, and real-time availability. If you have access to URLs or web links associated with these recommendations, please provide them when available."
+    )
+
 
 if 'messages' not in st.session_state.keys():
     st.session_state.messages = [
