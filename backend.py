6d93a77767eba3e232be896ed59b70b76b19d234
M Saqlain <saqlain2204@gmail.com>
2023-09-18 12:51:01 +0530

    Initial commit

---
+++ backend.py
@@ -0,0 +1,22 @@
+from llama_index import SimpleDirectoryReader, VectorStoreIndex, ServiceContext
+
+from llama_index.llms import OpenAI
+import openai
+
+def indexing() -> None:
+    """
+    Parameters: None
+
+    The function converts the data to whcih the LLM is grounded to vector indices and stores it in a json format in ./storage folder. The data from this folder is extracted in the main.py file
+
+    Returns: None
+    """
+    openai.api_key='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'
+    docs = SimpleDirectoryReader('./data').load_data()
+    # print(len(docs))
+    service_context = ServiceContext.from_defaults(llm=OpenAI(temperature=0.8))
+    index = VectorStoreIndex.from_documents(documents=docs, service_context=service_context)
+    index.storage_context.persist()
+    print("data stored in ./storage folder")
+
+indexing()
