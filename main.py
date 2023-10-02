4db3895326b412f780c5ffe843777cde99d80309
M Saqlain <118016760+saqlain2204@users.noreply.github.com>
2023-10-02 23:07:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import streamlit as st
 
 hide_streamlit_style = """
             <style>
-            data-testid #stDecoration {display: none;}
+            .st-emotion-cache-zq5wmm ezrtsby0 {display: none;}
             footer {visibility: hidden;}
             </style>
             """
