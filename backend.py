b0ab643da3fbef6f881a807babec6f4a5818827b
M Saqlain <118016760+saqlain2204@users.noreply.github.com>
2023-10-02 18:42:59 +0530

    Update backend.py

--- backend.py
+++ backend.py
@@ -14,7 +14,7 @@ def indexing() -> None:
     openai.api_key = st.secrets['key']
     docs = SimpleDirectoryReader('./data').load_data()
     # print(len(docs))
-    service_context = ServiceContext.from_defaults(llm=OpenAI(temperature=0.8))
+    service_context = ServiceContext.from_defaults(llm=OpenAI(temperature=0.6, prompt=f"Assist in offering tailored hotel recommendations based on user inquiries, taking into account the desired location, budget constraints, and individual preferences. Ensure to present the most suitable options with comprehensive details regarding amenities, user reviews, and real-time availability. If you have access to URLs or web links associated with these recommendations, please provide them when available."))
     index = VectorStoreIndex.from_documents(documents=docs, service_context=service_context)
     index.storage_context.persist()
     print("data stored in ./storage folder")
